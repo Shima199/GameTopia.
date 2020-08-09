0882bc9e7130a11844714ce8e9d2864215c35f5b
Shima199 <61685352+Shima199@users.noreply.github.com>
2020-08-09 22:33:10 +0800

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#btnfirst").click(function(){
+    $("#main1").fadeToggle();
+  });
+  $("#btnsecond").click(function(){
+    $("#main2").fadeToggle();
+  });
+  $("#btnthird").click(function(){
+    $("#main3").fadeToggle();
+  });
+  $("#btnfourth").click(function(){
+    $("#main4").fadeToggle();
+  });
+  $("#btnfifth").click(function(){
+    $("#main5").fadeToggle();
+  });
+});
